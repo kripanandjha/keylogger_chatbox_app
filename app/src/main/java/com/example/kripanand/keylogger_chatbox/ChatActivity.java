47e3bd863ed1e416a55ea8049cfdb05ed8649fc9
VinuthCS <vinuthcs90@yahoo.com>
2016-10-30 18:31:32 -0700

    Screens for login and new account

---
+++ app/src/main/java/com/example/kripanand/keylogger_chatbox/ChatActivity.java
@@ -0,0 +1,118 @@
+package com.example.kripanand.keylogger_chatbox;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+
+public class ChatActivity extends AppCompatActivity {
+
+    private EditText message;
+    private Button sendMessageButton;
+    public static final String USER_ID_KEY = "userId";
+    private String currentUserId;
+    private ListView listView;
+    private ArrayList<Message> mMessages;
+    private ChatAdapter mAdapter;
+    private Handler handler = new Handler() {
+        @Override
+        public void publish(LogRecord record) {
+
+        }
+
+        @Override
+        public void flush() {
+
+        }
+
+        @Override
+        public void close() throws SecurityException {
+
+        }
+    };
+    private static final int MAX_MSG_TO_SHOW = 70;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+        getCurrentUser();
+    }
+
+    private void getCurrentUser(){
+        currentUserId = ParseUser.getCurrentUser().getObjectId();
+        messagePosting();
+
+    }
+
+    private void messagePosting() {
+
+        message = (EditText) findViewById(R.id.newMessage);
+        sendMessageButton = (Button) findViewById(R.id.buttonSend);
+        listView = (ListView) findViewById(R.id.listciew_chat);
+        mMessages = new ArrayList<Message>();
+        mAdapter = new ChatAdapter(ChatActivity.this,currentUserId,mMessages);
+        listView.setAdapter(mAdapter);
+
+        sendMessageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!message.getText().toString().equals("")){
+                    Message msg = new Message();
+                    msg.setUserID(currentUserId);
+                    msg.setBody(message.getText().toString());
+                    msg.saveInBackground(new SaveCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            receiveMessages();
+                        }
+                    });
+                    message.setText("");
+                }else{
+                    Toast.makeText(getApplicationContext(), "Empty Message", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void receiveMessages() {
+        ParseQuery<Message> query = ParseQuery.getQuery(Message.class);
+        query.setLimit(MAX_MSG_TO_SHOW);
+        query.orderByAscending("createdAt");
+
+        query.findInBackground(new FindCallback<Message>() {
+            @Override
+            public void done(List<Message> messages, ParseException e) {
+                if(e == null){
+                    mMessages.clear();
+                    mMessages.addAll(messages);
+                    mAdapter.notifyDataSetChanged();
+                    listView.invalidate();
+                }else{
+                    Log.v("Error:","Error:" + e.getMessage());
+                }
+
+            }
+        });
+    }
+
+    private void refreshMessages(){
+        receiveMessages();
+    }
+}
---
+++ app/src/main/java/com/example/kripanand/keylogger_chatbox/NewAccountActivity.java
@@ -0,0 +1,110 @@
+package com.example.kripanand.keylogger_chatbox;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+import com.dd.processbutton.FlatButton;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+public class NewAccountActivity extends AppCompatActivity {
+    private EditText emailAddress;
+    private EditText phoneNumber;
+    private EditText userName;
+    private EditText password1;
+    private EditText password2;
+    private FlatButton newAccountBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_account);
+
+        emailAddress = (EditText) findViewById(R.id.EmailId);
+        phoneNumber = (EditText) findViewById(R.id.MobileId);
+        userName = (EditText) findViewById(R.id.NameId);
+        password1 = (EditText) findViewById(R.id.Password1Id);
+        password2 = (EditText) findViewById(R.id.Password2Id);
+        newAccountBtn = (FlatButton) findViewById(R.id.CreateIdBtn);
+
+        newAccountBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                newAccount();
+            }
+        });
+
+    }
+
+    private void newAccount(){
+        final String uEmail = emailAddress.getText().toString();
+        final String uPhone = phoneNumber.getText().toString();
+        final String uName = userName.getText().toString();
+        final String uPassword1 = password1.getText().toString();
+        final String uPassword2 = password2.getText().toString();
+
+        if (uEmail.equals("") || uPhone.equals("") || uName.equals("") || uPassword1.equals("") || uPassword2.equals("")){
+            AlertDialog.Builder dialog = new AlertDialog.Builder(NewAccountActivity.this);
+            dialog.setTitle("Empty Fields");
+            dialog.setMessage("Please enter all fields");
+            dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+            dialog.show();
+        }else{
+            ParseUser user = new ParseUser();
+
+            //set core properties
+            user.setUsername(uPhone);
+            user.setPassword(uPassword1);
+            user.setEmail(uEmail);
+
+            //set custom properties
+            user.put("Name",uName);
+
+            user.signUpInBackground(new SignUpCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e == null){
+                        emailAddress.setEnabled(false);
+                        phoneNumber.setEnabled(false);
+                        userName.setEnabled(false);
+                        password1.setEnabled(false);
+                        password2.setEnabled(false);
+                        newAccountBtn.setEnabled(false);
+                        loguserin(uPhone,uPassword1);
+                    }
+                }
+            });
+        }
+
+    }
+
+    private void loguserin(String uPhone, String uPassword1) {
+        if (!uPhone.equals("") || !uPassword1.equals("")){
+            ParseUser.logInInBackground(uPhone, uPassword1, new LogInCallback() {
+                @Override
+                public void done(ParseUser user, ParseException e) {
+                    if (e == null){
+                        startActivity(new Intent(NewAccountActivity.this, ChatActivity.class));
+
+                    }else{
+
+                    }
+                }
+            });
+        }else{
+
+        }
+    }
+}

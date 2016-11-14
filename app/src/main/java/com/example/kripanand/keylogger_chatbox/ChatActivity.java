0a24e34f0af8d0bfc223221ba9961a05a680d5b4
VinuthCS <vinuthcs90@yahoo.com>
2016-11-13 17:35:02 -0800

    Screens for login and new account

--- app/src/main/java/com/example/kripanand/keylogger_chatbox/ChatActivity.java
+++ app/src/main/java/com/example/kripanand/keylogger_chatbox/ChatActivity.java
@@ -1,9 +1,12 @@
 package com.example.kripanand.keylogger_chatbox;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,6 +14,7 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import com.parse.FindCallback;
+import com.parse.LogOutCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
@@ -113,5 +117,39 @@ public class ChatActivity extends AppCompatActivity {
 
     private void refreshMessages(){
         receiveMessages();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu logout) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_chat, logout);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_logout) {
+
+            ParseUser.logOutInBackground(new LogOutCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e == null){
+                        startActivity(new Intent(ChatActivity.this,MainActivity.class));
+                    }else{
+
+                    }
+                }
+            });
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
